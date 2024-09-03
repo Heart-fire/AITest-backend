8a4b2f780c0d91fd32f2fbf653145d81361787e7
Heart-fire <wangchunbo384@gmail.com>
2024-09-03 10:31:02 +0800

    @Heart-fire@接入AI模块并封装

---
+++ src/main/java/com/aitest/springbootinit/manager/AiManager.java
@@ -0,0 +1,104 @@
+package com.aitest.springbootinit.manager;
+
+import com.aitest.springbootinit.common.ErrorCode;
+import com.aitest.springbootinit.exception.BusinessException;
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sun.org.apache.xml.internal.serializer.utils.Utils.messages;
+
+/**
+ * 通用 AI 调用能力
+ */
+@Component
+public class AiManager {
+
+    @Resource
+    private ClientV4 clientV4;
+
+    // 稳定的随机数
+    private static final float STABLE_TEMPERATURE = 0.05f;
+
+    // 不稳定的随机数
+    private static final float UNSTABLE_TEMPERATURE = 0.99f;
+
+    /**
+     * 同步调用（答案较稳定）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncStableRequest(String systemMessage, String userMessage) {
+        return doSyncRequest(systemMessage, userMessage, STABLE_TEMPERATURE);
+    }
+
+    /**
+     * 同步调用（答案较随机）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncUnstableRequest(String systemMessage, String userMessage) {
+        return doSyncRequest(systemMessage, userMessage, UNSTABLE_TEMPERATURE);
+    }
+
+
+    public String doSyncRequest(String systemMessage, String userMessage, Float temperature) {
+        return doRequest(systemMessage, userMessage, Boolean.FALSE, temperature);
+    }
+
+    /**
+     * 通用请求(简化消息传递)
+     * @param systemMessage
+     * @param userMessage
+     * @param stream
+     * @param temperature
+     * @return
+     */
+    public String doRequest(String systemMessage,String userMessage,Boolean stream, Float temperature) {
+        ArrayList<ChatMessage> chatMessageList = new ArrayList<>();
+        ChatMessage syetemChatMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), systemMessage);
+        ChatMessage userChatMessage = new ChatMessage(ChatMessageRole.USER.value(), userMessage);
+        chatMessageList.add(syetemChatMessage);
+        chatMessageList.add(userChatMessage);
+        return doRequest(chatMessageList,stream,temperature);
+    }
+    /**
+     * 通用请求
+     * @param messages
+     * @param stream
+     * @param temperature
+     * @return
+     */
+    public String doRequest(List<ChatMessage> messages, Boolean stream, Float temperature) {
+        // 构造请求
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(stream)
+                .invokeMethod(Constants.invokeMethod)
+                .temperature(temperature)
+                .messages(messages)
+                .build();
+        ModelApiResponse invokeModelApiResp = clientV4.invokeModelApi(chatCompletionRequest);
+        try{
+            ChatMessage result = invokeModelApiResp.getData().getChoices().get(0).getMessage();
+            return result.getContent().toString();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,e.getMessage());
+        }
+
+    }
+
+}
---
+++ src/main/java/com/aitest/springbootinit/scoring/KeyConstant.java
@@ -0,0 +1,5 @@
+package com.aitest.springbootinit.scoring;
+
+public interface KeyConstant {
+   public static final String KEY = "98ee8b5159f7c65ea53dba7a0c88e2d9.0PuEnWnUPjgv5qMm";
+}
